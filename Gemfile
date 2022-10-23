e633125378e720fa34023aa4a1e5ac964ed453fc
Rafael Lugli <lugli.rafael@gmail.com>
2022-10-23 14:58:43 -0300

    Allow receive requests from external domain

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
 gem 'rswag-api', '~> 2.7'
 gem 'rswag-ui', '~> 2.7'
+gem 'rack-cors'
 
 group :development, :test do
   gem 'debug', platforms: %i[mri mingw x64_mingw]
--- config/application.rb
+++ config/application.rb
@@ -31,9 +31,7 @@ module OctoEvents
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.hosts << ENV.fetch('WEBHOOK_DOMAIN')
   end
 end
